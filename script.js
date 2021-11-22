9f071f71bb00288f3920d9fd8a73a2832bfceb4b
gleb <derrainaho@gmail.com>
2021-11-22 14:58:39 +0300

    Practic_2

--- script.js
+++ script.js
@@ -7,20 +7,28 @@ const personalMovieDB={
     genres: [],
     privat: false
 };
-
-const a = propmt('Один из последних просмотренных фильмов',''),
-      b = prompt('На сколко оценьте его?', ''),
-      c = prompt('Один из последних просмотренных фильмов',''),
-      d = prompt('На сколко оценьте его?', '');
-
-personalMovieDB.movies[a]=b;
-personalMovieDB.movies[c]=d;
+for(let i= 0; i<2;i++){
+    const a = prompt('Один из последних просмотренных фильмов',''),
+    b = prompt('На сколко оценьте его?', '');
+    if(a !=null && b !=null && a != '' && b != '' && a != a.lenght<50){
+        personalMovieDB.movies[a]=b;
+        console.log('done');
+    } else{
+        console.log('Error');
+        i--;
+    }
+}
+if(personalMovieDB.count<10){
+    console.log('Просмотрено довольно мало фильмов');
+} else if(personalMovieDB.count>=10 && personalMovieDB.count<30){
+    console.log('Вы классный зритель');
+} else if (personalMovieDB.count>=30){
+    console.log('Вы киноман');
+}else{
+    console.log('Произошла ошибка');
+}
 concole.log(personalMovieDB);
   
-
-
-
-
 
 
 
